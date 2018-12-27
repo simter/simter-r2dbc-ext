57fc146877155396d918a8f9f18d714a4a1a2442
RJ <rongjihuang@gmail.com>
2018-12-27 14:19:09 +0800

    Add MssqlConnectionFactoryConfiguration

---
+++ src/main/kotlin/tech/simter/r2dbc/mssql/MssqlConnectionFactoryConfiguration.kt
@@ -0,0 +1,34 @@
+package tech.simter.r2dbc.mssql
+
+import io.r2dbc.mssql.MssqlConnectionConfiguration
+import io.r2dbc.mssql.MssqlConnectionFactory
+import io.r2dbc.spi.ConnectionFactory
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import tech.simter.r2dbc.R2dbcProperties
+
+@Configuration
+@ConditionalOnClass(name = ["io.r2dbc.mssql.MssqlConnectionConfiguration"])
+open class MssqlConnectionFactoryConfiguration @Autowired constructor(
+  private val properties: R2dbcProperties
+) {
+  private val logger = LoggerFactory.getLogger(MssqlConnectionFactoryConfiguration::class.java)
+
+  init {
+    if (logger.isWarnEnabled) logger.warn("R2dbcProperties={}", properties.copy(password = "***"))
+  }
+
+  @Bean
+  open fun connectionFactory(): ConnectionFactory {
+    val builder = MssqlConnectionConfiguration.builder()
+    properties.database?.let { builder.database(it) }
+    properties.host?.let { builder.host(it) }
+    properties.username?.let { builder.username(it) }
+    properties.password?.let { builder.password(it) }
+    properties.port?.let { builder.port(it) }
+    return MssqlConnectionFactory(builder.build())
+  }
+}
