eda32ac85e7e20576e1610ff5b7a35456f210a62
RJ <rongjihuang@gmail.com>
2019-01-02 19:04:55 +0800

    Concat all sql script content and run it together

--- src/main/kotlin/tech/simter/r2dbc/R2dbcConfiguration.kt
+++ src/main/kotlin/tech/simter/r2dbc/R2dbcConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
 import org.springframework.util.FileCopyUtils
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.io.InputStreamReader
 import javax.annotation.PostConstruct
@@ -29,16 +30,20 @@ open class R2dbcConfiguration(
     // initial database by execute SQL script through R2dbcProperties.schema|initializationMode config
     val resourcePatternResolver = PathMatchingResourcePatternResolver()
     if (!properties.schema.isNullOrEmpty() && properties.initializationMode != NEVER) {
-      //Flux.fromIterable(properties.schema)
-      properties.schema?.forEach {
-        logger.info("Executing SQL script from $it")
-        val sql = FileCopyUtils.copyToString(InputStreamReader(resourcePatternResolver.getResource(it).inputStream))
-        Mono.from(connectionFactory().create())
-          .flatMapMany { c -> c.createStatement(sql).execute() }
-          .log()
-          .then()
-          .subscribe()
-      }
+      Flux.fromIterable(properties.schema!!)
+        .map { FileCopyUtils.copyToString(InputStreamReader(resourcePatternResolver.getResource(it).inputStream)) }
+        .doOnError { e -> logger.error(e.message, e) }
+        .collectList()
+        .map { it.joinToString("\r\n") } // concat all sql script content
+        .doOnSuccess {
+          if (logger.isInfoEnabled) logger.info("Executing SQL script from ${properties.schema}")
+          if (logger.isDebugEnabled) logger.debug(it)
+        }.flatMap {
+          Mono.from(connectionFactory().create())
+            .flatMapMany { c -> c.createStatement(it).execute() }
+            .doOnError { e -> logger.error(e.message, e) }
+            .then(Mono.just(true))
+        }.subscribe()
     }
   }
 }
