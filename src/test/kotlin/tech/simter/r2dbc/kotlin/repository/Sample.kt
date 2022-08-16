362e433e6b1633d825177c13d2c25e7f8d660000
RJ <rongjihuang@gmail.com>
2022-08-16 11:18:49 +0800

    Add test for repository.save

--- src/test/kotlin/tech/simter/r2dbc/kotlin/repository/Sample.kt
+++ src/test/kotlin/tech/simter/r2dbc/kotlin/repository/Sample.kt
@@ -1,18 +1,8 @@
 package tech.simter.r2dbc.kotlin.repository
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.domain.Persistable
 
 data class Sample(
-  @JvmField
   @Id val id: Int,
   val name: String
-) : Persistable<Int> {
-  override fun isNew(): Boolean {
-    return true
-  }
-
-  override fun getId(): Int {
-    return id
-  }
-}
+)
--- src/test/kotlin/tech/simter/r2dbc/kotlin/repository/SampleRepository.kt
+++ src/test/kotlin/tech/simter/r2dbc/kotlin/repository/SampleRepository.kt
@@ -2,4 +2,4 @@ package tech.simter.r2dbc.kotlin.repository
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 
-interface SampleRepository : R2dbcRepository<Sample, String>
+interface SampleRepository : R2dbcRepository<Sample, Int>
